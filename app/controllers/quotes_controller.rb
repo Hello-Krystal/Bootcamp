3f79dfe9018e773be942113f21ccb74222bc8ba6
Krystal <krystal.raysor@gmail.com>
2019-08-05 00:00:23 +0000

    added linkss to pages

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,21 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to_root_path
+  end
+
+  private
+
+  def quote_params
+    params.requite(:quote).permit(:saying, :author)
+  end
+
 end
+
